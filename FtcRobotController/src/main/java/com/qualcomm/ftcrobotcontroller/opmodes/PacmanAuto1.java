bf06084184221296a145196ef9ffbbfc65ba911a
Bennett Liu <bennettliu@Bennetts-MacBook-Air.local>
2015-11-18 22:51:30 -0800

    Add easier timing changes for experimentation

--- FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/PacmanAuto1.java
+++ FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/PacmanAuto1.java
@@ -4,14 +4,24 @@ import com.qualcomm.robotcore.util.ElapsedTime;
 
 public class PacmanAuto1 extends PacmanBotHardwareBase {
     final static VersionNumber version = new VersionNumber(1,0,0);
-    boolean set=false;
+    boolean done=false;
     ElapsedTime timer=new ElapsedTime();
     @Override
     public void init() {
-        telemetry.addData("Program","Pacman Auto");
-        telemetry.addData("Version",version.string());
+        telemetry.addData("Program", "Pacman Auto");
+        telemetry.addData("Version", version.string());
         telemetry.addData("Hardware Base Version", hwbVersion.string());
         setupHardware();
+        double timeDiffs[];
+        timeDiffs = new double[3];//amount of time distances
+        timeDiffs[0]=2;//time operating 1st instruction
+        timeDiffs[1]=1;//time operating 2nd instruction
+        timeDiffs[2]=2;//time operating 3rd instruction
+        int x=0;
+        for(int i=0;i<2;i++){
+            x+=timeDiffs[i];
+            timeDiffs[i]=x;
+        }
         //setSweeperPosition(0);
     }
 
@@ -20,18 +30,23 @@ public class PacmanAuto1 extends PacmanBotHardwareBase {
         //Drive the robot
         /*double drive_rate = 100;
         double turn_rate  = 0;*/
-        if(!set)
+
+        if(!done)
         {
-            set=true;
+            done=true;
             timer.startTime();
         }
-        if(timer.time()<3)
+        if(timer.time()<timeDiffs[0])//1st instuction
         {
             drive(1,0);
         }
-        else if(timer.time()<6)
+        else if(timer.time()<timeDiffs[1])//2nd instruction
         {
             drive(0,1);
+        }
+        else if(timer.time()<timeDiffs[2])//3rd instruction
+        {
+            drive(1,0);
         }
         else
         {
