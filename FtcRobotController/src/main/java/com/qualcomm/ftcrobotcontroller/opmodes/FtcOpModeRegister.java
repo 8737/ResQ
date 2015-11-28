eb0ed580f075340789b824126c2040e717de9057
ThomasDoylend <tmdoylend@gmail.com>
2015-11-28 10:29:25 -0800

    Removed HookMonitor

--- FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/FtcOpModeRegister.java
+++ FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/FtcOpModeRegister.java
@@ -65,7 +65,7 @@ public class FtcOpModeRegister implements OpModeRegister {
     //manager.register ("PushBotManual", PushBotManual.class);
     //manager.register("Manual Drive",PacmanBotManualFinal.class);
     //manager.register("Pacman Auto",PacmanAuto1.class);
-    manager.register("Hook Monitor",PacmanBotHookMonitor.class);
+    //manager.register("Hook Monitor",PacmanBotHookMonitor.class);
     manager.register("Color Sensor Test",ColorSensorTest.class);
     manager.register("Thing",MRRGBExample.class);
 
